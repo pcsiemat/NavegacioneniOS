26e9ae14b2d4cc113c79e6aa42727e826f3407c0
alumno <alumno@a51-02.local>
2017-01-31 19:37:52 -0500

    Initial Commit

---
+++ Navagation en IOS 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navagation en IOS 2
+//
+//  Created by alumno on 31/01/17.
+//  Copyright © 2017 alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
